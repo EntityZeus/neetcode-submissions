870344658812f52acc5e7bf47a9708fa3bf6867a
chandrashekhar maity <57037635+EntityZeus@users.noreply.github.com>
2026-06-17 17:07:42 +0530

    Add: two-integer-sum-ii - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-1.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} numbers
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(numbers, target) {
+        let left = 0, right = numbers.length - 1;
+
+        while(left < right) {
+            const sum = numbers[left] + numbers[right];
+            if(sum > target) {
+                right = right - 1;
+            } else if( sum < target) {
+                left = left + 1;
+            } else {
+                return [left+1, right+1];
+            }
+        }
+    }
+}

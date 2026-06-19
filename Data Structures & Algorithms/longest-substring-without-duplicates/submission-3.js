90ad4a02d7298018081c647bd820c2569de857a0
chandrashekhar maity <57037635+EntityZeus@users.noreply.github.com>
2026-06-19 16:46:05 +0530

    Add: longest-substring-without-duplicates - submission-3

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-3.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    lengthOfLongestSubstring(s) {
+        if(s.length === 0) return 0;
+        if(s.length === 1) return 1;
+
+        let left = 0, right = 0;
+        let map = {};
+        let res = 0;
+        while(right < s.length) {
+            if(s[right] in map && map[s[right]] >= left) {
+                let diff = right - left;
+                res = Math.max(diff, res);
+                left = map[s[right]] + 1;
+                map[s[right]] = right;
+            } else {
+                map[s[right]] = right;
+            }
+            right+=1;
+        }
+        return Math.max(res, right - left)
+    }
+}

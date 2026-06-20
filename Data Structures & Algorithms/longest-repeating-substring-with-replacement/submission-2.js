ac3f63882eea7a4788a916f601fd0b59bc33c614
chandrashekhar maity <57037635+EntityZeus@users.noreply.github.com>
2026-06-20 12:40:16 +0530

    Add: longest-repeating-substring-with-replacement - submission-2

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-2.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {number} k
+     * @return {number}
+     */
+    characterReplacement(s, k) {
+        
+        let map = {};
+        let maxFreq = 0;
+        let left = 0;
+        let res = 0;
+        for(let right = 0; right < s.length; right ++) {
+            map[s[right]] = (map[s[right]] || 0) + 1;
+            maxFreq = Math.max(maxFreq, map[s[right]]);
+            // adjust left if window size is exceding req
+            while(right - left + 1 - maxFreq > k ) {
+                map[s[left]] -=1;
+                left ++;
+            }
+            res = Math.max(res, right - left + 1);
+        }
+        return res;
+    }
+}
